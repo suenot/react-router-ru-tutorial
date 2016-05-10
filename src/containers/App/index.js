bd3b58faf94abcdf05cb60e82dc5004fc483c48f
Eugen Soloviov <suenot@gmail.com>
2016-05-10 09:06:42 +0300

    3.3 refactor, active link

---
+++ src/containers/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import NavLink from '../../components/NavLink'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className='container'>
+                <ul className='nav nav-pills'>
+                    <li><NavLink to='/admin'>Админка</NavLink></li>
+                    <li><NavLink to='/list'>Список жанров</NavLink></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
+}
