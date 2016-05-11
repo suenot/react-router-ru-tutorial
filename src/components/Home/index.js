6faacb623c6b3a8c68ddcfe6be227d78dcb1e229
Eugen Soloviov <suenot@gmail.com>
2016-05-11 07:07:38 +0300

    3.6

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div className='row'>
-        <div className='col-md-12'>Раздел /</div>
-      </div>
-    )
-  }
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        const value = e.target.elements[0].value.toLowerCase()
+        this.context.router.push(`/genre/${value}`)
+    }
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-md-12'>Раздел /</div>
+                <form className='col-md-4' onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='genreName'/>
+                    <button type='submit'>Перейти</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+Home.contextTypes = {
+    router: PropTypes.object.isRequired
 }
